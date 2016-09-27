86b20b6d13d0321bc477dffb8e5a082cce384182
VANDERHEYDEN, Chris <c.vanderheyden@evs.com>
2016-09-27 18:12:35 +0200

    Env config vars

--- configuration/config.js
+++ configuration/config.js
@@ -1,6 +1,12 @@
 function Config(){
-    if(this instanceof Config){
-        this.cs = process.env.CS.replace(/"/g,"");              
+    if(this instanceof Config){    
+        if(process.env.CS!==undefined && process.env.CS !== null){
+            console.log('CS ENVIRONMENT VARIABLE: %s', process.env.CS);
+            this.cs = process.env.CS.replace(/"/g,"");    
+        }
+        else{
+            console.error('CS ENVIRONMENT MISSING');
+        }
         return this;
     }        
     else {
